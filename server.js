8d3b5fbe35256ed525d2f038285b23c2d8a8a63c
Bruce Tsvil <superunknown1103@gmail.com>
2017-08-07 14:18:30 -0500

    setup server.js

--- server.js
+++ server.js
@@ -7,4 +7,15 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
