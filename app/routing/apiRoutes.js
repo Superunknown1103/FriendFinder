72906c7fc7f6c64a9952dad94df90f2840b83c00
Bruce Tsvil <superunknown1103@gmail.com>
2017-08-10 09:31:09 -0500

    updated apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,13 @@
 var friends = require("..data/friends");
 
 module.exports = function (app) {
-	app.get('/api/friends', function (req, res){
-		res.json(friends);
-	});
+	app.get('/api/friends', function(req, res){
+	//res.send("Welcome to the Friend Finder!")
+	res.sendFile(path.join(__dirname + '/.html'));
+});
 
-
-	app.post('/api/friends', function (req, res) {
-		// 
-	});
+app.post('api/friends', function(req, res){
+	//res.send("Welcome to the Friend Finder!")
+	res.sendFile(path.join(__dirname + '/home.html'));
+});
 }
