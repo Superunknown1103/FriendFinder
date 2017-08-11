f227f46a2f47b50618feccbfbabdf8ef5dcdc510
Bruce Tsvil <superunknown1103@gmail.com>
2017-08-11 10:11:27 -0500

    almost done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,4 +13,40 @@ app.post('/api/friends', function(req, res){
 
 	console.log(friends)
 });
+
+
+app.post("/api/friends", function(req, res) {
+	var request = req.body;
+	console.log(request);
+
+var friendScore = 50;
+var bestFriend;
+var tempFriendScore = 0;
+
+for (var i=0; i < friends.length; i++) {
+	for (var j=0; j < friends[i].scores.length; j++) {
+		var temp = Math.abs(friends[i].scores[j] - request.scores[j]);
+		tempFriendScore = tempFriendScore + temp;
+		if (tempFriendScore < friendScore) {
+			friendScore = tempFriendScore;
+			bestFriend = friends[i];
+			console.log("Best friend:")
+			console.log(bestFriend);
+		}
+	}
+	console.log("Friend Score:")
+	console.log(tempFriendScore);
+	tempFriendScore = 0;
+}
+friendScore = 50;
+friends.push(request);
+res.json(bestFriend);
+console.log("Array of all friends:")
+console.log(friends);
+});
+
+
+app.post("/api/clear", function() {
+  friends = [];
+});
 };
